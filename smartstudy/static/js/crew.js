317d0c4203afd100158447b65e6b776d9438f20a
PerhapsSPY <py@smartstudy.co.kr>
2013-05-02 16:47:52 +0900

    모집 안내 페이지 슬그머니 넣음
    입사하지않겠나?

--- smartstudy/static/js/crew.js
+++ smartstudy/static/js/crew.js
@@ -1,10 +1,14 @@
 function random_crew(number) {
-    var $crew_li = $('ul.crew li');
+    var $crew_li = $('ul.crew li.crew');
     if (number===undefined) {var number = Math.floor((Math.random()*$crew_li.size())-1);}
     var $crew_first = $crew_li.eq(number).html();
     $('div.crew_first').empty().append($crew_first);
 }
-$('ul.crew li').hover(function () {
+$('ul.crew li.crew').hover(function () {
     random_crew($(this).index());
 });
 random_crew();
+
+$('ul.crew li.recruit').click(function() {
+    $('#recruit').slideDown(1000);
+});
