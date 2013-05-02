ee1942a52759a0ca865315518e82b4849c377bcb
PerhapsSPY <py@smartstudy.co.kr>
2013-05-02 15:55:14 +0900

    crew 랜덤 첫번째 호버 버그 수정

--- smartstudy/static/js/crew.js
+++ smartstudy/static/js/crew.js
@@ -1,12 +1,10 @@
 function random_crew(number) {
     var $crew_li = $('ul.crew li');
-    if (!number) {var number = Math.floor((Math.random()*$crew_li.size())-1);}
+    if (number===undefined) {var number = Math.floor((Math.random()*$crew_li.size())-1);}
     var $crew_first = $crew_li.eq(number).html();
     $('div.crew_first').empty().append($crew_first);
 }
-random_crew();
-
 $('ul.crew li').hover(function () {
     random_crew($(this).index());
 });
-
+random_crew();
