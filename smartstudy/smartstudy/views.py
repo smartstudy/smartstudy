4a3d5cec104cdac497821bb1a8975fc0e647c97e
PerhapsSPY <py@smartstudy.co.kr>
2013-05-02 14:29:35 +0900

    view.py 충돌 수정

--- smartstudy/smartstudy/views.py
+++ smartstudy/smartstudy/views.py
@@ -5,15 +5,10 @@ from django.shortcuts import render
 
 class HomeView(TemplateView):
     template_name = 'home/main.html'
-<<<<<<< HEAD
-
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['crew'] = {}
+        return context
 
 def privacy(request):
     return render(request, 'privacy.html')
-=======
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        
-        context['crew'] = {}
-        return context
->>>>>>> crew 초안만듬
