52e10d1de71bf500b8361328f56f7ce9628df72a
Eunchong Yu <kroisse@gmail.com>
2013-05-02 11:57:28 +0900

    Add subtemplates

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
 import flask
+from flask import render_template
 
 app = flask.Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_reloader=True)
