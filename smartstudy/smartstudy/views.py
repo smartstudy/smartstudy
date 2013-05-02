d12301bd400456076ca5de82a57eaa8b209d5655
PerhapsSPY <py@smartstudy.co.kr>
2013-05-02 14:46:53 +0900

    crew 임시로 뜨게해봄

--- smartstudy/smartstudy/views.py
+++ smartstudy/smartstudy/views.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
+from crew.models import Crew
 
 
 class HomeView(TemplateView):
     template_name = 'home/main.html'
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['crew'] = {}
+        context['crew'] = Crew.objects.all() 
         return context
 
 def privacy(request):
