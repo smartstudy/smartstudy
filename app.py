ffe74857b08f9d09db93e499e6aa377e804bbf8c
Eunchong Yu <kroisse@gmail.com>
2013-05-02 12:15:42 +0900

    Add more footer and privacy.html

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def home():
     return render_template('main.html')
 
 
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
